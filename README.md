86659061722df5d7acefe9de76fee6b05ca041d2
Red3tar <you@example.com>
2025-10-29 10:52:37 -0700

    Updated to new website format

--- README.md
+++
@@ -1,1 +0,0 @@
-# VSiD-website
--- app.js
+++ app.js
@@ -1,24 +1,25 @@
-function toggleMenu() {
-    const navbarLinks = document.getElementById('navbar-links');
-    navbarLinks.style.display = navbarLinks.style.display === 'flex' ? 'none' : 'flex';
-}
+particlesJS.load('particles-js', 'particles.json', function() {
+    console.log('callback - particles.js config loaded');
+  });
 
-window.addEventListener('resize', function() {
-    const navbarLinks = document.getElementById('navbar-links');
-    const hamburgerIcon = document.getElementById('hamburger-icon');
-    
-    if (window.innerWidth > 1000) {
-        navbarLinks.style.display = 'flex';
-        hamburgerIcon.style.display = 'none';
-    } else {
-        navbarLinks.style.display = 'none';
-        hamburgerIcon.style.display = 'block';
+  $('.owl-carousel').owlCarousel({
+    loop: true,
+    margin: 10,
+    autoplay: true,
+    autoplayTimeout: 1500,
+    autoplaySpeed: 800,
+    nav: false,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 3
+        },
+        1000: {
+            items: 5
+        }
     }
 });
 
-function toggleText() {
-    const missionText = document.querySelector("#mission-section h4");
-    const button = document.getElementById("toggle-button");
-    missionText.classList.toggle("expanded");
-    button.textContent = missionText.classList.contains("expanded") ? "Read Less" : "Read More";
-}
+
